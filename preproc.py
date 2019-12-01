db336c57fe33eb962423eb60ec550f3585064b47
Clayygou <1784489871@qq.com>
2019-12-01 16:07:52 +0800

    Add files via upload
    annotation

--- preproc.py
+++ preproc.py
@@ -4,7 +4,11 @@ import numpy as np
 import torchvision.transforms as transforms
 
 
+#对训练集和验证集做不同的数据增强，得到train和valid data
+
+
 class Cutout(object):
+    # 随机遮挡图片，如果不触及边缘，遮挡面积最大为（length x length）
     def __init__(self, length):
         self.length = length
 
@@ -20,7 +24,7 @@ class Cutout(object):
         x2 = np.clip(x + self.length // 2, 0, w)
 
         mask[y1: y2, x1: x2] = 0.
-        mask = torch.from_numpy(mask)
+        mask = torch.from_numpy(mask) # 转换成tensor
         mask = mask.expand_as(img)
         img *= mask
 
@@ -33,32 +37,32 @@ def data_transforms(dataset, cutout_length):
         MEAN = [0.49139968, 0.48215827, 0.44653124]
         STD = [0.24703233, 0.24348505, 0.26158768]
         transf = [
-            transforms.RandomCrop(32, padding=4),
-            transforms.RandomHorizontalFlip()
+            transforms.RandomCrop(32, padding=4),  # 随机裁剪 变成32X32
+            transforms.RandomHorizontalFlip()      # 0.5概率水平翻转
         ]
     elif dataset == 'mnist':
         MEAN = [0.13066051707548254]
         STD = [0.30810780244715075]
         transf = [
-            transforms.RandomAffine(degrees=15, translate=(0.1, 0.1), scale=(0.9, 1.1), shear=0.1)
+            transforms.RandomAffine(degrees=15, translate=(0.1, 0.1), scale=(0.9, 1.1), shear=0.1)  # 保持中心不变的对图片进行随机仿射变化
         ]
     elif dataset == 'fashionmnist':
         MEAN = [0.28604063146254594]
         STD = [0.35302426207299326]
         transf = [
             transforms.RandomAffine(degrees=15, translate=(0.1, 0.1), scale=(0.9, 1.1), shear=0.1),
-            transforms.RandomVerticalFlip()
+            transforms.RandomVerticalFlip()      # 0.5概率垂直翻转 猜的     
         ]
     else:
         raise ValueError('not expected dataset = {}'.format(dataset))
 
     normalize = [
-        transforms.ToTensor(),
-        transforms.Normalize(MEAN, STD)
+        transforms.ToTensor(),             # 转换到0-1之间
+        transforms.Normalize(MEAN, STD)    # 归一化（-1到1）之间
     ]
 
     train_transform = transforms.Compose(transf + normalize)
-    valid_transform = transforms.Compose(normalize)
+    valid_transform = transforms.Compose(normalize)             # 验证集只进行归一化处理
 
     if cutout_length > 0:
         train_transform.transforms.append(Cutout(cutout_length))
